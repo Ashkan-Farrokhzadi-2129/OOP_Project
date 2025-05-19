714826e8a8a2d1b61778e79cf4d9e740060db49c
Ashkan Farrokhzadi <a.farrokhzadi2129@gmail.com>
2025-05-19 23:31:24 +0330

    Change nodes in Edge to Node object pointers

--- Model/Edge.cpp
+++ Model/Edge.cpp
@@ -6,7 +6,7 @@
 
 
 // Constructor
-Edge::Edge(const std::string& type, const std::string& id, int node1, int node2, double value)
+Edge::Edge(const std::string& type, const std::string& id, Node* node1, Node* node2, double value)
     : type(type), id(id), node1(node1), node2(node2), value(value) {}
 
 // Getters
@@ -18,11 +18,11 @@ std::string Edge::getId() const {
     return id;
 }
 
-int Edge::getNode1() const {
+Node* Edge::getNode1() const {
     return node1;
 }
 
-int Edge::getNode2() const {
+Node* Edge::getNode2() const {
     return node2;
 }
 
@@ -39,11 +39,11 @@ void Edge::setId(const std::string& id) {
     this->id = id;
 }
 
-void Edge::setNode1(int node1) {
+void Edge::setNode1(Node* node1) {
     this->node1 = node1;
 }
 
-void Edge::setNode2(int node2) {
+void Edge::setNode2(Node* node2) {
     this->node2 = node2;
 }
 
--- Model/Edge.h
+++ Model/Edge.h
@@ -8,32 +8,33 @@
 
 
 #include <string>
+#include "Node.h"
 
 class Edge {
 protected:
     std::string type;     // نوع المان (مثلا "Resistor", "Capacitor")
     std::string id;       // شناسه یا نام المان
-    int node1;            // شماره گره اول گره مثبت
-    int node2;            // شماره گره دوم گره منفی
+    Node* node1;            // شماره گره اول گره مثبت
+    Node* node2;            // شماره گره دوم گره منفی
     double value;         // مقدار المان (مثلا مقاومت، ظرفیت و ...)
 
 public:
     // سازنده (کانستراکتور)
-    Edge(const std::string& type, const std::string& id, int node1, int node2, double value);
+    Edge(const std::string& type, const std::string& id, Node* node1, Node* node2, double value);
     virtual ~Edge() = default;
 
     // Getters
     std::string getType() const;
     std::string getId() const;
-    int getNode1() const;
-    int getNode2() const;
+    Node* getNode1() const;
+    Node* getNode2() const;
     double getValue() const;
 
     // Setters
     void setType(const std::string& type);
     void setId(const std::string& id);
-    void setNode1(int node1);
-    void setNode2(int node2);
+    void setNode1(Node* node1);
+    void setNode2(Node* node2);
     void setValue(double value);
 };
 
