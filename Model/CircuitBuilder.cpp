3719518a711336344b0f1e7a93a454bede6a1b28
Ashkan Farrokhzadi <a.farrokhzadi2129@gmail.com>
2025-05-21 23:10:51 +0330

    CircuitBuilder class creation

---
+++ Model/CircuitBuilder.cpp
@@ -0,0 +1,5 @@
+//
+// Created by asus on 5/21/2025.
+//
+
+#include "CircuitBuilder.h"
---
+++ Model/CircuitBuilder.h
@@ -0,0 +1,16 @@
+//
+// Created by asus on 5/21/2025.
+//
+
+#ifndef CIRCUITBUILDER_H
+#define CIRCUITBUILDER_H
+
+
+
+class CircuitBuilder {
+
+};
+
+
+
+#endif //CIRCUITBUILDER_H
