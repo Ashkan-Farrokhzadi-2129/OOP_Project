a7f060ab2e23dfcd90e70b133c569f54324496ab
Ashkan Farrokhzadi <a.farrokhzadi2129@gmail.com>
2025-05-21 21:57:00 +0330

    Stamp override for voltage source

--- Model/VoltageSource.cpp
+++ Model/VoltageSource.cpp
@@ -1,8 +1,33 @@
 #include "VoltageSource.h"
+#include "CircuitMatrix.h"
+#include "Node.h"
 
-VoltageSource::VoltageSource(const std::string& id, Node* node1, Node* node2, double voltage)
-    : Edge("VoltageSource", id, node1, node2, voltage) {}
+VoltageSource::VoltageSource(const std::string& id, Node* node1, Node* node2, double voltage, int matrixIndex)
+    : Edge("VoltageSource", id, node1, node2, voltage), matrixIndex(matrixIndex) {}
 
 std::string VoltageSource::getType() const {
     return "VoltageSource";
+}
+
+void VoltageSource::stamp(CircuitMatrix& matrix) {
+    int pos = node1->getNumber();
+    int neg = node2->getNumber();
+    int vsIdx = getMatrixIndex(); // You need to provide this index when creating the voltage source
+
+    // B and C matrices
+    if (pos >= 0) {
+        matrix.B(pos, vsIdx) = 1;
+        matrix.C(vsIdx, pos) = 1;
+    }
+    if (neg >= 0) {
+        matrix.B(neg, vsIdx) = -1;
+        matrix.C(vsIdx, neg) = -1;
+    }
+
+    // E vector
+    matrix.E(vsIdx) = value;
+}
+
+int VoltageSource::getMatrixIndex() const {
+    return matrixIndex;
 }
--- Model/VoltageSource.h
+++ Model/VoltageSource.h
@@ -6,9 +6,16 @@
 
 class VoltageSource : public Edge {
 public:
-    VoltageSource(const std::string& id, Node* node1, Node* node2, double voltage);
+    VoltageSource(const std::string& id, Node* node1, Node* node2, double voltage, int matrixIndex);
 
     std::string getType() const override;
+    int getMatrixIndex() const;
+
+    void stamp(CircuitMatrix& matrix) override;
+
+private:
+    int matrixIndex; // Index of this voltage source in the MNA matrix
 };
+
 
 #endif // VOLTAGESOURCE_H
