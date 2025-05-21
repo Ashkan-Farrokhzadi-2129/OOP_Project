c0fa0610e8e51fe0d972154ac57e11bac47f2a72
Ashkan Farrokhzadi <a.farrokhzadi2129@gmail.com>
2025-05-21 23:13:28 +0330

    CircuitBuilder class creation

--- Model/CircuitBuilder.cpp
+++ Model/CircuitBuilder.cpp
@@ -1,5 +1,38 @@
-//
-// Created by asus on 5/21/2025.
-//
-
 #include "CircuitBuilder.h"
+
+CircuitBuilder::CircuitBuilder() : voltageSourceCount(0) {}
+
+Node* CircuitBuilder::getOrCreateNode(int nodeNumber) {
+    auto it = nodes.find(nodeNumber);
+    if (it != nodes.end()) {
+        return it->second;
+    }
+    Node* node = new Node(nodeNumber);
+    nodes[nodeNumber] = node;
+    return node;
+}
+
+void CircuitBuilder::addResistor(const std::string& id, int n1, int n2, double value) {
+    Node* node1 = getOrCreateNode(n1);
+    Node* node2 = getOrCreateNode(n2);
+    edges.push_back(new Resistor(id, node1, node2, value));
+}
+
+void CircuitBuilder::addVoltageSource(const std::string& id, int n1, int n2, double value) {
+    Node* node1 = getOrCreateNode(n1);
+    Node* node2 = getOrCreateNode(n2);
+    edges.push_back(new VoltageSource(id, node1, node2, value, voltageSourceCount));
+    voltageSourceCount++;
+}
+
+void CircuitBuilder::addCurrentSource(const std::string& id, int n1, int n2, double value) {
+    Node* node1 = getOrCreateNode(n1);
+    Node* node2 = getOrCreateNode(n2);
+    edges.push_back(new CurrentSource(id, node1, node2, value));
+}
+
+CircuitMatrix CircuitBuilder::buildMatrix() {
+    CircuitMatrix matrix(static_cast<int>(nodes.size()), voltageSourceCount);
+    matrix.assemble(edges);
+    return matrix;
+}
--- Model/CircuitBuilder.h
+++ Model/CircuitBuilder.h
@@ -1,16 +1,35 @@
-//
-// Created by asus on 5/21/2025.
-//
-
 #ifndef CIRCUITBUILDER_H
 #define CIRCUITBUILDER_H
 
-
+#include <unordered_map>
+#include <vector>
+#include <string>
+#include "Node.h"
+#include "Edge.h"
+#include "Resistor.h"
+#include "VoltageSource.h"
+#include "CurrentSource.h"
+#include "CircuitMatrix.h"
 
 class CircuitBuilder {
+public:
+    CircuitBuilder();
 
+    Node* getOrCreateNode(int nodeNumber);
+
+    void addResistor(const std::string& id, int n1, int n2, double value);
+    void addVoltageSource(const std::string& id, int n1, int n2, double value);
+    void addCurrentSource(const std::string& id, int n1, int n2, double value);
+
+    CircuitMatrix buildMatrix();
+
+    const std::vector<Edge*>& getEdges() const;
+    int getVoltageSourceCount() const;
+
+private:
+    std::unordered_map<int, Node*> nodes;
+    std::vector<Edge*> edges;
+    int voltageSourceCount;
 };
-
-
 
 #endif //CIRCUITBUILDER_H
