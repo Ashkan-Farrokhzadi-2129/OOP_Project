b172629b0c9dd2d4584e76549897ac99ca3eeba7
Ashkan Farrokhzadi <a.farrokhzadi2129@gmail.com>
2025-05-25 11:24:05 +0330

    Capacitor stamp

--- Model/Elements/Capacitor.cpp
+++ Model/Elements/Capacitor.cpp
@@ -1,8 +1,42 @@
 #include "Capacitor.h"
+#include "CircuitMatrix.h"
+#include "Node.h"
+#include <cmath>
 
 Capacitor::Capacitor(const std::string& id, Node* node1, Node* node2, double capacitance)
     : Edge("Capacitor", id, node1, node2, capacitance) {}
 
 std::string Capacitor::getType() const {
     return "Capacitor";
+}
+
+void Capacitor::stamp(CircuitMatrix& matrix) {
+    // DC stamp: open circuit (no stamping)
+}
+
+
+void Capacitor::updateStamps(double dt, CircuitMatrix& matrix, const Eigen::VectorXd& voltages) {
+    int n1 = node1->getNumber() - 1; // assuming nodes are 1-based
+    int n2 = node2->getNumber() - 1;
+
+    // Companion model: G_eq = 2C/dt, I_eq = - (2C/dt)*V_prev - I_prev
+    double G_eq = (2.0 * value) / dt;
+    double I_eq = -G_eq * prevVoltage - prevCurrent;
+
+    // Stamp the conductance
+    if (n1 >= 0) matrix.G(n1, n1) += G_eq;
+    if (n2 >= 0) matrix.G(n2, n2) += G_eq;
+    if (n1 >= 0 && n2 >= 0) {
+        matrix.G(n1, n2) -= G_eq;
+        matrix.G(n2, n1) -= G_eq;
+    }
+
+    // Stamp the current source
+    if (n1 >= 0) matrix.J(n1) -= I_eq;
+    if (n2 >= 0) matrix.J(n2) += I_eq;
+
+    // Update previous values for next time step
+    double currentVoltage = (n1 >= 0 ? voltages(n1) : 0) - (n2 >= 0 ? voltages(n2) : 0);
+    prevCurrent = G_eq * currentVoltage + I_eq;
+    prevVoltage = currentVoltage;
 }
--- Model/Elements/Capacitor.h
+++ Model/Elements/Capacitor.h
@@ -9,6 +9,14 @@ public:
     Capacitor(const std::string& id, Node* node1, Node* node2, double capacitance);
 
     std::string getType() const override;
+    void stamp(CircuitMatrix& matrix) override;
+
+    // For time-domain analysis
+    void updateStamps(double dt, CircuitMatrix& matrix, const Eigen::VectorXd& voltages);
+
+private:
+    double prevCurrent = 0.0;
+    double prevVoltage = 0.0;
 };
 
 #endif // CAPACITOR_H
